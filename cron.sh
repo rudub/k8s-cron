c24bd527610185decbceb530fe363e89dc2e060d
rudub <35490403+rudub@users.noreply.github.com>
2022-02-05 22:23:52 +0530

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
+color=$1
 cron_name=`kubectl get cronjob | grep auto-delete`
-
+echo $color
 if [[ -n "$cron_name" ]]; then
   echo "cron job found deleting it......"
   kubectl delete cronjob $cron_name
 else
   echo "no cron job found creating new....."
-  if [[$deploy_name == "blue"]]; then
-    kubectl create cronjob auto-delete --image=busybox '--schedule=0 */12 * * *' -- echo 'kubectl delete deploy example'  
+  if [[ "$color" == "blue" ]]; then
+    echo "deployment found Blue...."
+    kubectl create cronjob auto-delete --image=busybox '--schedule=0 * * * *' -- echo 'kubectl delete deploy nginx-green'  
   else
-    kubectl create cronjob auto-delete --image=busybox '--schedule=0 */12 * * *' -- echo 'kubectl delete deploy example1'
+    echo "deployment found green.."
+    kubectl create cronjob auto-delete --image=busybox '--schedule=0 * * * *' -- echo 'kubectl delete deploy nginx-blue'
   fi
 fi
+kubectl get pods
 echo $cron_name
 
 
