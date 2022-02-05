0cdc53dde4018dbd1bbaef0dfc5a5bbec8158e16
rudub <35490403+rudub@users.noreply.github.com>
2022-02-05 19:27:54 +0530

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-$cron_name=`kubectl get cronjob`
-kubectl create cronjob auto-delete --image=busybox   --schedule="*/1 * * * *" -- echo "kubectl delete deploy example"
+$cron_name=`kubectl get cronjob | grep auto-delete`
+
+if [[ -n "$cron_name" ]]; then
+  echo "cron job found deleting it......"
+  kubectl delete cronjob $cron_name
+else
+  echo "no cron job found creating new....."
+  kubectl cronjob auto-delete --image=busybox '--schedule=* * * * *' -- echo 'kubectl delete deploy example'  
 echo $cron_name
+
+
